286affadf94f497a16176665a75a5215c43fbe41
Pier Luc Ducharme <dutch80@gmail.com>
2025-10-01 09:43:52 -0400

    Ajout clarifications for semaine 4

--- Semaine 4/Exemples/boucle_for_2.py
+++ Semaine 4/Exemples/boucle_for_2.py
@@ -1,8 +1,21 @@
 
 # for s'attend à parcourir une liste/séquence
+
+# Dans ce cas-ci, une liste
 fruits = ["fraises", "framboises", "bleuets"]
 
 for fruit in fruits:
     print(fruit)
+
+# Une séquence de nombre générée par range
+for i in range(1, 10):
+    print(i)
+
+# Une str est une chaîne de caractères, donc une liste de caractères. La parcourir avec une boucle for va itérer sur les
+# caractères
+phrase = "J'aime le python!"
+for caractere in phrase:
+    print(caractere)
+
 
 
